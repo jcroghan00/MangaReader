ad8c77a14c41ad7ae931a30b10fd6a91a0bd98c7
Jack Croghan <jcroghan@iastate.edu>
2023-06-25 05:56:30 -0500

    Most of the data for new chapters is now filled in correctly

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿namespace MangaReader;
 
 using Flurl;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.IO;
 using System.Text.Json.Nodes;
-using System.Xml.Linq;
 
 public partial class MainPage : ContentPage
 {
@@ -13,22 +15,22 @@ public partial class MainPage : ContentPage
 	static string coverBaseUrl = "https://uploads.mangadex.org/covers/";
 
     private JsonNode popularNewManga;
-    private byte[][] popularNewImages;
+    private byte[][] popularNewImages = new byte[10][];
+
+    private JsonNode recentChapters;
 
     public MainPage()
 	{
 		InitializeComponent();
 
-        popularNewImages = new byte[10][];
-
 		getPopularNew();
-        getNewReleases();
+        setNewReleases();
 	}
 
     private async void getPopularNew()
 	{
         var dt = DateTime.Now;
-        dt = dt.AddDays(-14);
+        dt = dt.AddDays(-31);
 
         // Used Flurl for Url building since it was difficult to fill in the rather complex queries that
         // mangadex expects with c#s default Url building
@@ -41,7 +43,7 @@ public partial class MainPage : ContentPage
 
         // This kinda sucks but it will be how I have to do orderings here and in the future since mangadex
         // expects an object as a parameter and both Flurl and c# don't make that easy
-		baseUrl += "order%5BfollowedCount%5D=desc";
+        url += "&order%5BfollowedCount%5D=desc";
 
         HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Get, url);
         HttpResponseMessage res = await client.SendAsync(msg);
@@ -140,6 +142,7 @@ public partial class MainPage : ContentPage
 
         var tags = popularNewManga[popularNewIndex]["attributes"]["tags"].AsArray();
 
+        // Adds a tag for the content rating of a manga if the manga is not 'safe'
         if (popularNewManga[popularNewIndex]["attributes"]["contentRating"].ToString() != "safe")
         {
             Border newTag = makeNewTag(popularNewManga[popularNewIndex]["attributes"]["contentRating"].ToString());
@@ -164,9 +167,154 @@ public partial class MainPage : ContentPage
         }
     }
 
-    private async void getNewReleases()
+    private async Task<JsonNode> getNewReleases()
     {
-        return;
+        var url = baseUrl
+            .AppendPathSegment("chapter")
+            .SetQueryParam("limit", 15)
+            .SetQueryParam("contentRating[]", new[] { "safe", "suggestive" })
+            .SetQueryParam("includes[]", new[] { "manga", "scanlation_group" })
+            .SetQueryParam("translatedLanguage[]", "en");
+
+        url += "&order%5BreadableAt%5D=desc"; // readableAt = asc
+
+        HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Get, url);
+        HttpResponseMessage res = await client.SendAsync(msg);
+        res.EnsureSuccessStatusCode();
+
+        var jNode = JsonNode.Parse(res.Content.ReadAsStream());
+        recentChapters = jNode["data"];
+
+        return recentChapters;
+    }
+
+    private async void setNewReleases()
+    {
+        JsonNode data = await getNewReleases();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Border border = new Border
+            {
+                BackgroundColor = (Color)App.Current.Resources["bgColor"],
+                Stroke = (Color)App.Current.Resources["bgColor"],
+                StrokeThickness = 0,
+                HeightRequest = (newChapterStack1.Height - 20 - (4 * 5)) / 5,
+                Margin = new Thickness(5, 2.5, 5, 2.5),
+                StrokeShape = new RoundRectangle
+                {
+                    CornerRadius = 5
+                }
+            };
+
+            Grid grid = new Grid
+            {
+                BackgroundColor = (Color)App.Current.Resources["bgColor"],
+                ColumnDefinitions =
+                    {
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) },
+                        new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+                    },
+                RowDefinitions =
+                    {
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                        new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
+                    },
+            };
+
+            Border imageBorder = new Border
+            {
+                BackgroundColor = (Color)App.Current.Resources["bgColor"],
+                Stroke = (Color)App.Current.Resources["bgColor"],
+                StrokeThickness = 0,
+                HeightRequest = (newChapterStack1.Height - 20 - (4 * 5)) / 5,
+                Margin = new Thickness(5, 5, 5, 5),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                WidthRequest = 80,
+                StrokeShape = new RoundRectangle
+                {
+                    CornerRadius = 5
+                },
+                Content = new Image
+                {
+                    Source = "manga_test.jpg",
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center
+                }
+            };
+
+            Grid.SetRowSpan(imageBorder, 3);
+            grid.Add(imageBorder, 0, 0);
+
+            var chapterRelations = data[i]["relationships"].AsArray();
+
+            string mangaTitle = "Manga Title";
+            string scanGroup = "No Group";
+
+            FontAttributes scanGroupAttributes = FontAttributes.Italic;
+
+            for (int j = 0; j < chapterRelations.Count(); j++)
+            {
+
+                if (chapterRelations[j]["type"].ToString() == "manga")
+                {
+                    mangaTitle = chapterRelations[j]["attributes"]["title"]["en"].ToString();
+                }
+                else if (chapterRelations[j]["type"].ToString() == "scanlation_group")
+                {
+                    scanGroup = chapterRelations[j]["attributes"]["name"].ToString();
+                    scanGroupAttributes = FontAttributes.None;
+                }
+            }
+
+            grid.Add(new Label 
+            {
+                Text = mangaTitle,
+                LineBreakMode = LineBreakMode.TailTruncation,
+                FontAttributes = FontAttributes.Bold,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Start
+            }, 1, 0);
+
+            string chapterText = data[i]["attributes"]["volume"] != null ? $"Vol. {data[i]["attributes"]["volume"]} " : "";
+            chapterText += $"Ch. {data[i]["attributes"]["chapter"]}";
+            chapterText += data[i]["attributes"]["title"] != null ? $" - {data[i]["attributes"]["title"]}" : "";
+
+            grid.Add(new Label 
+            { 
+                Text = chapterText,
+                LineBreakMode = LineBreakMode.TailTruncation,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Start
+            }, 1, 1);
+
+            grid.Add(new Grid
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Padding = new Thickness(0, 0, 10, 0),
+                Children =
+                {
+                    new Label
+                    {
+                        Text = scanGroup,
+                        FontAttributes = scanGroupAttributes,
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Start
+                    },
+                    new Label
+                    {
+                        Text = "Time Readable",
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.End
+                    }
+                }
+            }, 1, 2);
+
+            border.Content = grid;
+            newChapterStack1.Add(border);
+        }
     }
 
     private Border makeNewTag(string tag)
@@ -177,7 +325,7 @@ public partial class MainPage : ContentPage
         if (tag == "suggestive" || tag == "erotica")
         {
             text = tag[0].ToString().ToUpper() + tag.Substring(1);
-            backgroundColor = (Color)App.Current.Resources[tag + "BgColor"];
+            backgroundColor = (Color) App.Current.Resources[tag + "BgColor"];
         }
 
         Border border = new Border
@@ -226,4 +374,3 @@ public partial class MainPage : ContentPage
         popularNewTitle.Text = "hehe";
     }
 }
-
